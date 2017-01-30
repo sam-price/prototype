581175d0e1ffdbf207b54dc1561ef72da42a42d0
sam-price <samprice.ethos@gmail.com>
2017-01-30 08:29:24 +0000

    Deployment

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(first_name: "Boris the Dog", email: "samprice.ethos@gmail.com", password: "Pricetag305", password_confirmation: "Pricetag305")
+User.create(first_name: "Isaac", last_name: 'Lucksted', email: 'isaac@example.com', password: "Pricetag305", password_confirmation: "Pricetag305", gym: 'Horfield Leisure Center')
+User.create(first_name: "Boris the Dog", email: "samprice.ethos@gmail.com", password: "Pricetag305", password_confirmation: "Pricetag305", gym: 'The Park')
 User.create(first_name: "Jack", email: "jack@example.com", password: "Pricetag305", password_confirmation: "Pricetag305")
 User.create(first_name: "Hollie", email: "hollie@example.com", password: "Pricetag305", password_confirmation: "Pricetag305")
 User.create(first_name: "Becca", email: "becca@example.com", password: "Pricetag305", password_confirmation: "Pricetag305")
